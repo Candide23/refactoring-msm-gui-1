17db984d4583f4521a4f1731d7150fa6ed877d29
Candide Mboungou-Kimpolo <mboungoukimpolo@gmail.com>
2025-04-29 10:13:50 -0500

    refactored code

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -13,4 +13,25 @@
 #  director_id :integer
 #
 class Movie < ApplicationRecord
+
+  def director
+
+    the_id = self.director_id
+
+    matching_directors = Director.where({ :id => the_id })
+        
+    the_director = matching_directors.at(0)
+
+    return  the_director
+    
+  end
+
+  def characters
+    my_id = self.id
+
+    matching_characters =Character.where({:movie_id => my_id})
+
+    return matching_characters
+    
+  end
 end
